571172ad3b30e3461536d496e7a8288f59849483
FarahKamleh <fkamleh8800@gmail.com>
2023-03-14 16:49:00 -0500

    Door Functionalities
    When the box collider is touched, sound is triggered and Try Another appears. The door's animation also triggers. Revealing the path causes an issue with the collider where the player cannot enter the door. In addition, the sound needs a delay.

---
+++ CS 427 - Count (ME)NA In/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    // identify the collider
+    Collider boxCollider;
+
+    // door gameobject
+    public GameObject door;
+
+    // other path and scholarship circle
+    public GameObject otherPath;
+    public GameObject scholCirc;
+
+    // audio
+    public AudioSource open;
+
+    // flag to ensure sound plays once
+    bool soundFlag = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // fetch the collider
+        boxCollider = GetComponent<Collider>();
+
+        // stop animation at start
+        door.GetComponent<Animator>().enabled = false;
+    }
+
+    // if collides
+    private void OnTriggerEnter(Collider other)
+    {
+        // open door
+        door.GetComponent<Animator>().enabled = true;
+
+        // add the path and next circle
+        otherPath.SetActive(true);
+        scholCirc.SetActive(true);
+
+        // play sound once
+        if (soundFlag == true)
+        {
+            // play sound
+            open.Play();
+
+            // make false
+            soundFlag = false;
+        }
+    }
+}
