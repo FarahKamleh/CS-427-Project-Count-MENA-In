d5e618287480ef935738268e8f0ace16b13cfe98
FarahKamleh <fkamleh8800@gmail.com>
2023-03-14 23:19:11 -0500

    Door Sound Delay
    A delay has been added before the opening door sound. It is now in synch. All the paths appear after opening which is temporary and only for video demonstration.

--- CS 427 - Count (ME)NA In/Assets/Scripts/OpenDoor.cs
+++ CS 427 - Count (ME)NA In/Assets/Scripts/OpenDoor.cs
@@ -14,6 +14,12 @@ public class OpenDoor : MonoBehaviour
     public GameObject otherPath;
     public GameObject scholCirc;
 
+    // FIXME: remove this later
+    public GameObject whitePath;
+    public GameObject reportCirc;
+    public GameObject commPath;
+    public GameObject finalCirc;
+
     // audio
     public AudioSource open;
 
@@ -40,14 +46,30 @@ public class OpenDoor : MonoBehaviour
         otherPath.SetActive(true);
         scholCirc.SetActive(true);
 
+        // FIXME: remove later
+        whitePath.SetActive(true);
+        reportCirc.SetActive(true);
+        commPath.SetActive(true);
+        finalCirc.SetActive(true);
+
         // play sound once
         if (soundFlag == true)
         {
-            // play sound
-            open.Play();
+            // create a delay before the sound plays
+            StartCoroutine(Delay());
 
             // make false
             soundFlag = false;
         }
+    }
+
+    // delay function
+    IEnumerator Delay()
+    {
+        // wait 1 seconds
+        yield return new WaitForSeconds(1);
+
+        // play sound
+        open.Play();
     }
 }
