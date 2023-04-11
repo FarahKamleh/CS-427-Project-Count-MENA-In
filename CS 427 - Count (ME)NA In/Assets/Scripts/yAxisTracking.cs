f8a36fe09b87718ef10f74afe66c606493310f45
FarahKamleh <fkamleh8800@gmail.com>
2023-04-10 21:09:49 -0500

    Y Axis Tracking
    A new script has been added to ensure only the y axis is considered by adding all typography to a cube simulating the player.

---
+++ CS 427 - Count (ME)NA In/Assets/Scripts/yAxisTracking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class yAxisTracking : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // fetch rotation of head tracking
+        Vector3 headRotation = CAVE2.GetHeadRotation(1).eulerAngles;
+
+        // apply y rotation to cube
+        this.transform.localEulerAngles = new Vector3(0, headRotation.y, 0);
+    }
+}
